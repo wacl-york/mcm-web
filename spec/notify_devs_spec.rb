8e36c54f0adf07f6001d7791df6b268ef2cad0a3
Stuart Lacy <stuart.lacy@york.ac.uk>
2022-08-24 19:20:50 +0100

    Updated unit tests to match our environment
    Removed references to logged in users, dbs, and sending error messages

--- spec/notify_devs_spec.rb
+++ spec/notify_devs_spec.rb
@@ -11,23 +11,6 @@ RSpec.describe 'Check NotifyDevs is set up correctly' do
 
   let(:error_url) { '/test-error' }
 
-  context 'when the error handler is run' do
-    # In the test environment, raise_errors is enabled, which allows the exceptions to propogate outside of the app,
-    # so rspec can check for them. We can disable this to allow the app's error handler to handle the error instead.
-    # We must also pretend we're in a lambda; we only call NotifyDevs when we're in a lambda.
-    before do
-      allow(FacultyAWS::NotifyDevs).to receive :send_error_warning
-      allow(LOGGER).to receive(:error)
-      allow(Sinatra::Application).to receive(:in_lambda?).and_return(true)
-      allow(Sinatra::Application).to receive(:raise_errors?).and_return(false)
-    end
-
-    it 'will try to send an error warning' do
-      get error_url
-      expect(FacultyAWS::NotifyDevs).to have_received(:send_error_warning)
-    end
-  end
-
   # This test will check that the setup for disabling raise_errors does not leak into other spec tests.
   context 'when the test runs as normal' do
     it 'will raise the error' do
--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -2,10 +2,8 @@
 
 RSpec.describe 'Sample Spec Tests' do
   describe 'the index page' do
-    include_context 'log in as', 'abc123'
 
     before do
-      visit '/login'
       visit '/'
     end
 
@@ -17,18 +15,6 @@ RSpec.describe 'Sample Spec Tests' do
       expect(page).to have_link href: %r{/}
     end
 
-    it 'displays the currently logged in user' do
-      expect(page).to have_text 'abc123'
-    end
   end
 
-  describe 'the database page' do
-    before do
-      visit '/database'
-    end
-
-    it 'displays a list of available databases' do
-      expect(page).to have_text 'faculty'
-    end
-  end
 end
