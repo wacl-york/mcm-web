00f51fcb94aa8438a8bfb7391ce2f0ae70c52848
Stuart Lacy <stuart.lacy@york.ac.uk>
2023-02-09 10:45:58 +0000

    Route for finding front page species

---
+++ routes/browse.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+get '/browse' do
+
+  @categories = DB[:FrontPageSpecies]
+                .join(:Species, Name: :Species)
+                .to_hash_groups(:CompoundClass, [:Species, :HumanReadable, :Inchi])
+  erb :browse
+end
