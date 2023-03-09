74ce969961f1cd0a4a82b83fb495dae5f81a70a5
Stuart Lacy <stuart.lacy@york.ac.uk>
2023-03-09 15:06:45 +0000

    Get reaction as hierarchical structure

--- routes/species.rb
+++ routes/species.rb
@@ -1,24 +1,47 @@
 # frozen_string_literal: true
 
 get '/species/:species' do
-  @sink_reactions = DB[:Reactants]
-                    .where(Species: params[:species])
-                    .join(:ReactionsWide, [:ReactionID])
-                    .join(:Rates, [:Rate])
-                    .order(:Reaction)
-                    .select(:Reaction, :Rate, :RateType)
-
-  @precursor_reactions = DB[:Products]
-                         .where(Species: params[:species])
-                         .join(:ReactionsWide, [:ReactionID])
-                         .join(:Rates, [:Rate])
-                         .order(:Reaction)
-                         .select(:Reaction, :Rate, :RateType)
-
+  # For every species get a list of reactions they are either reactants in (sink)
+  # or products in (precursor), along with any relevant metadata
+  @sink_rxns = get_reactions(params[:species], column: :Reactants)
+  @precursor_rxns = get_reactions(params[:species], column: :Products)
   @species = DB[:Species]
              .where(Name: params[:species])
              .select(:Name, :Smiles, :Inchi, :Mass)
              .first
 
   erb :species
+end
+
+def get_reactions(species, column: :Reactants)
+  # Finds all reactions in the DB with a given species
+  # and parses into a hierarchical data structure of:
+  # [
+  #   {
+  #     ReactionID: <id>,
+  #     Rate: '<rate>',
+  #     ReactionCategory: '<category>',
+  #     Reactants: [...],
+  #     Products: [...]
+  #   }
+  # ]
+  # Find all reactionIDs that this species is involved in
+  ids = DB[column].where(Species: species).distinct.select(:ReactionID)
+
+  # Then extract all relevant information
+  reactants = DB[:Reactants].where(ReactionID: ids).to_hash_groups(:ReactionID)
+  products = DB[:Products].where(ReactionID: ids).to_hash_groups(:ReactionID)
+  rxns = DB[:Reactions].where(ReactionID: ids).to_hash(:ReactionID)
+
+  # And parse into the desired output format
+  # First map needed to turn in Ruby array rather than Sequel Dataset
+  ids.map(:ReactionID).map do |id|
+    {
+      Reaction: id,
+      Rate: rxns[id][:Rate],
+      Category: rxns[id][:RateCategory],
+      Products: products[id].map { |x| x[:Species] },
+      Reactants: reactants[id].map { |x| x[:Species] }
+    }
+  end
 end
