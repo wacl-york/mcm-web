131c7a1f8b8c188c5d574effc96c48293d83afde
Stuart Lacy <stuart.lacy@york.ac.uk>
2023-03-14 21:40:37 +0000

    Moved marklist JS code to own file

--- routes/add_marklist.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-post '/add_marklist/:species' do
-  session[:mark_list] = Set[] if session[:mark_list].nil?
-  session[:mark_list].add(params[:species])
-
-  redirect back
-end
--- routes/remove_marklist.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-post '/remove_marklist' do
-  session[:mark_list] = session[:mark_list] - params[:selected] unless params[:selected].nil?
-  redirect back
-end
