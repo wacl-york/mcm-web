4bc6089558f1fe8445095e168fe5529c6a217e89
Stuart Lacy <stuart.lacy@york.ac.uk>
2023-03-14 11:20:28 +0000

    Restructured reaction layout

--- helpers/helpers.rb
+++ helpers/helpers.rb
@@ -39,16 +39,20 @@ helpers do
   end
 
   def display_reaction(rxn, species_page)
-    "<div class='row rxn-row d-flex justify-content-center'>
-      <div class='col-sm-11'>
-        #{parse_multiple_species(rxn[:Reactants], species_page)}
-        #{parse_rate(rxn[:Rate])}
-        #{parse_multiple_species(rxn[:Products], species_page)}
+    "
+      <div class='rxn-container'>
+        <div class='col-sm-11'>
+          <div class='rxn-row'>
+            #{parse_multiple_species(rxn[:Reactants], species_page)}
+            #{parse_rate(rxn[:Rate])}
+            #{parse_multiple_species(rxn[:Products], species_page)}
+          </div>
+        </div>
+        <div class='rxn-category col-sm-1'>
+          <a href='/reaction_category?category=#{rxn[:Category]}&reactionid=#{rxn[:ReactionID]}'>Doc</a>
+        </div>
       </div>
-      <div class='col-sm-1 d-flex justify-content-end'>
-        <a href='/reaction_category?category=#{rxn[:Category]}&reactionid=#{rxn[:ReactionID]}'>Doc</a>
-      </div>
-    </div>"
+      "
   end
 
   def parse_rate(rate)
@@ -69,7 +73,7 @@ helpers do
     parsed = parsed.gsub(/TEMP/, 'T')
 
     # Use mhchem's ce environment for getting reaction arrow that stretches with rate
-    "\\(\\ce{->[#{parsed}]}\\)"
+    "<div class='rxn-rate'>\\(\\ce{->[#{parsed}]}\\)</div>"
   end
 
   def parse_multiple_species(values, species_page)
@@ -77,14 +81,14 @@ helpers do
     # to a compound's own page. MathJAX is used to format the text
     values
       .map { |x| create_link_from_species_name(x[:Name], x[:Category], species_page) }
-      .join(' + ')
+      .join(' <div class="rxn-plus"> + </div>')
   end
 
   def create_link_from_species_name(name, category, species_page)
     # Creates a link to a species page under 2 conditions:
     # 1) It is a VOC, and 2) it is not the species that the current
     # page is displaying.
-    if (category == 'VOC') && (name != species_page)
+    inner_tag = if (category == 'VOC') && (name != species_page)
       "<span class='mytooltip tooltip-effect-1'>
       <span class='tooltip-item'><a href='/species/#{name}'>#{name}</a></span>
       <span class='tooltip-content clearfix'><img class='tooltip-image' src='/species_images/#{name}.png'/>
@@ -92,6 +96,7 @@ helpers do
     else
       "<span>#{name}</span>"
     end
+    "<div>#{inner_tag}</div>"
   end
 
   def remove_spaces(input)
