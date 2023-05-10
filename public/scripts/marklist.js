d112f2ac45c994503f79ce77c3c81c5b4e7b32f0
Stuart Lacy <stuart.lacy@york.ac.uk>
2023-05-10 11:17:23 +0100

    Only content that overlaps with margin moves

--- public/scripts/marklist.js
+++ public/scripts/marklist.js
@@ -157,13 +157,13 @@ function populateExportMarklist() {
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function showMarklist() {
   document.getElementById("marklistSidebar").style.width = "250px";
-  document.getElementById("main").style.marginRight = "250px";
+  document.getElementById("main").style.paddingRight = "250px";
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function hideMarklist() {
-  document.getElementById("marklistSidebar").style.width = "0";
-  document.getElementById("main").style.marginRight = "auto";
+  document.getElementById("marklistSidebar").style.width = "0px";
+  document.getElementById("main").style.paddingRight = "0px";
 } 
 
 function toggleMarklist() {
