2fbc0c8a27231c1bff3977bcddab1494a14ce3a3
Stuart Lacy <stuart.lacy@york.ac.uk>
2023-07-11 19:16:46 +0100

    Browse page displays selected mechanism

--- routes/browse.rb
+++ routes/browse.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 get '/:mechanism?/browse/?' do
-  # TODO: redirect instead?
   @mechanism = params[:mechanism] || settings.DEFAULT_MECHANISM
 
   @categories = DB[:FrontPageSpecies]
+                .where(Mechanism: @mechanism)
                 .join(:Species, Name: :Species)
                 .to_hash_groups(:CompoundClass, %i[Species HumanReadable Inchi])
   @inorganic_categories = DB[:InorganicReactionCategories].map(:InorganicReactionCategory)
