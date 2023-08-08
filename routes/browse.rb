405e796761d92a860515a459a6e972acbf5e22ed
Stuart Lacy <stuart.lacy@york.ac.uk>
2023-08-08 15:24:30 +0100

    Display smiles alongside species on browse page

--- routes/browse.rb
+++ routes/browse.rb
@@ -4,7 +4,7 @@ get '/:mechanism/browse/?' do
   @categories = DB[:FrontPageSpecies]
                 .where(Mechanism: @mechanism)
                 .join(:Species, Name: :Species)
-                .to_hash_groups(:CompoundClass, %i[Species HumanReadable Inchi])
+                .to_hash_groups(:CompoundClass, %i[Species HumanReadable Smiles])
   @inorganic_categories = DB[:InorganicReactionCategories].map(:InorganicReactionCategory)
   @marklist = cookies[:marklist]
   @marklist = @marklist.nil? ? [] : @marklist.split(',')
