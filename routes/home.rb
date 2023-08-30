c7cfe088f5d1a024f50e658764621e349cd5967a
Stuart Lacy <stuart.lacy@york.ac.uk>
2023-08-30 14:21:13 +0100

    Added home page content

--- routes/home.rb
+++ routes/home.rb
@@ -1,9 +1,65 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/BlockLength
 get '/:mechanism/?' do
-  @links = {
-    '/export' => 'Export a mechanism subset'
-  }
+  @mechanism_details = DB[:Mechanisms]
+                       .where(Mechanism: params[:mechanism])
+                       .first
+  @n_species = DB[:SpeciesMechanisms]
+               .where(Mechanism: params[:mechanism])
+               .count
+  @n_reactions = DB[:Reactions]
+                 .where(Mechanism: params[:mechanism])
+                 .count
+
+  @institutions = case params[:mechanism]
+                  when 'MCM'
+                    [
+                      {
+                        url: 'https://www.york.ac.uk/',
+                        logo: '/static/logos/uoy_black.svg',
+                        alt: 'University of York logo'
+                      },
+                      {
+                        url: 'https://ncas.ac.uk/',
+                        logo: '/static/logos/ncas_colour.png',
+                        alt: 'NCAS logo'
+                      },
+                      {
+                        url: 'https://www.leeds.ac.uk/',
+                        logo: '/static/logos/uol_black.svg',
+                        alt: 'University of Leeds logo'
+                      }
+                    ]
+                  when 'CRI'
+                    [
+                      {
+                        url: 'https://www.york.ac.uk/',
+                        logo: '/static/logos/uoy_black.svg',
+                        alt: 'University of York logo'
+                      },
+                      {
+                        url: 'https://ncas.ac.uk/',
+                        logo: '/static/logos/ncas_colour.png',
+                        alt: 'NCAS logo'
+                      },
+                      {
+                        url: 'https://www.bristol.ac.uk/',
+                        logo: '/static/logos/bristol_OLD.gif',
+                        alt: 'University of Bristol logo'
+                      },
+                      {
+                        url: 'https://www.leeds.ac.uk/',
+                        logo: '/static/logos/uol_black.svg',
+                        alt: 'University of Leeds logo'
+                      }
+                    ]
+                  end
+
+  # Mechanism-specific content
+  fn = File.join('public', 'static', params[:mechanism], 'home.html')
+  @content = File.file?(fn) ? File.read(fn) : "<h1>Error</h1><p>Unknown page '#{fn}'.</p>"
 
   erb :home
 end
+# rubocop:enable Metrics/BlockLength
