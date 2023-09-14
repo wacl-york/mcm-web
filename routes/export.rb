f79283cbdaf3a61cce9ecabc52ab8fc4a2e4f323
Stuart Lacy <stuart.lacy@york.ac.uk>
2023-09-14 11:25:23 +0100

    Refactored export to be polymorphic

--- routes/export.rb
+++ routes/export.rb
@@ -6,24 +6,16 @@ end
 
 # rubocop:disable Metrics/BlockLength
 post '/:mechanism/export' do
+  # Traverse submechanism to obtain species involved and the reactions
   submech_species = traverse_submechanism(params[:selected], @mechanism)
   submech_rxns = get_reactions_from_species(submech_species, @mechanism)
 
   # Include all inorganic reactions and species if user requested
   if params[:inorganic]
-    inorganic_rxns = DB[:ReactionsWide].exclude(InorganicCategory: nil).select(:ReactionID, :Reaction,
-                                                                               :Rate).from_self(alias: :inorg)
+    inorg_submech = extract_inorganic_submechanism
     # Can safely do a union all as we explicitly didn't include inorganic rxns in the initial submechanism extract
-    submech_rxns = inorganic_rxns.union(submech_rxns, all: true)
-
-    inorganic_reactants = submech_rxns
-                          .join(:Reactants, [:ReactionID])
-                          .select(:Species)
-    inorganic_products = submech_rxns
-                         .join(:Products, [:ReactionID])
-                         .select(:Species)
-    inorganic_species = inorganic_reactants.union(inorganic_products)
-    submech_species = inorganic_species.union(submech_species)
+    submech_rxns = inorg_submech[:rxns].union(submech_rxns, all: true)
+    submech_species = inorg_submech[:species].union(submech_species)
   end
 
   # Get Peroxy information
@@ -45,93 +37,17 @@ post '/:mechanism/export' do
              .where(PeroxyRadical: true)
   missing_peroxies = submech_species
                      .where(PeroxyRadical: nil)
-  peroxy_out = wrap_lines(peroxies.map(:Name),
-                          starting_char: 'RO2 = ',
-                          ending_char: ';',
-                          sep: ' + ',
-                          max_line_length: 65,
-                          every_line_start: ' ' * 6)
 
-  # Make available to download
-  content_type 'text/plain'
-  attachment 'mcm_export.fac'
-
-  # Format sections for export
-  species_out = wrap_lines(submech_species.map(:Name))
-  rxns_out = submech_rxns.map { |row| "% #{row[:Rate]} : #{row[:Reaction]} ;\n" }.join
-  complex_rates_out = complex_rates.map { |row| "#{row[:Child]} = #{row[:Definition]} ;\n" }.join
-
-  params_out = wrap_lines(params[:selected],
-                          starting_char: '* ',
-                          every_line_start: '* ',
-                          every_line_end: ' ;',
-                          ending_char: ' ;',
-                          sep: ' ')
-
-  missing_peroxies_out = wrap_lines(missing_peroxies.map(:Name),
-                                    starting_char: '* ',
-                                    every_line_start: '* ',
-                                    every_line_end: ' ;',
-                                    ending_char: ' ;',
-                                    sep: ' ')
-
-  spacer = "#{'*' * 77} ;\n"
-  empty_comment = "*;\n"
-
-  citation_file = File.open("#{settings.public_folder}/citation.txt")
-  citation_lines = citation_file.readlines.map(&:chomp)
-
-  #---------------------- Write Facsimile file
-  # Citation comes first
-  out = ''
-  out += spacer
-  out += citation_lines.map { |row| "* #{row}\n" }.join
-  out += spacer
-
-  # Selected species + all species in this mechanism
-  out += spacer
-  out += params_out
-  out += empty_comment
-  out += "* Variable definitions. All species are listed here.;\n"
-  out += empty_comment
-  out += "VARIABLE\n"
-  out += species_out
-  out += spacer
-
-  # Complex rate coefficients
-  if params[:generic]
-    out += empty_comment
-    out += "* Generic Rate Coefficients ;\n"
-    out += empty_comment
-    out += "* Complex reactions ;\n"
-    out += empty_comment
-    out += complex_rates_out
-  end
-
-  # Peroxies
-  if peroxies.count.positive?
-    out += spacer
-    out += "* Peroxy radicals. ;\n*;\n"
-    if missing_peroxies.count.positive?
-      out += "* WARNING: The following species do not have SMILES strings in the database. ;\n"
-      out += "*          If any of these are peroxy radicals the RO2 sum will be wrong!!! ;\n"
-      out += missing_peroxies_out # TODO: Shoud this exclude inorganics?
-    end
-    out += spacer
-    out += empty_comment
-    out += peroxy_out
-    out += empty_comment
-  end
-
-  # Reactions
-  out += "* Reaction definitions. ;\n"
-  out += empty_comment
-  out += rxns_out
-  out += empty_comment
-
-  # Summary
-  out + "* End of Subset. No. of Species = #{submech_species.count}, No. of Reactions = #{submech_rxns.count} ;"
-  #---------------------- End write facsimile file
+  export_func = exporter_factory(params[:format])
+  export_func.call(
+    submech_species.select_map(:Name),
+    submech_rxns.all,
+    complex_rates.all,
+    params[:selected],
+    missing_peroxies.select_map(:Name),
+    peroxies.select_map(:Name),
+    generic: params[:generic]
+  )
 end
 # rubocop:enable Metrics/BlockLength
 
@@ -181,7 +97,7 @@ def get_reactions_from_species(species, mechanism)
   #   - Sequel dataset with 3 columns: ReactionID, Reaction, Rate
   species
     .select_append(Sequel.lit('row_number() over() AS i'))
-    .from_self(alias: :ord)  # Needed to ensure row_number applied at correct time
+    .from_self(alias: :ord) # Needed to ensure row_number applied at correct time
     .join(:Reactants, [:Species])
     .join(:Species, Name: Sequel[:Reactants][:Species])
     .join(:ReactionsWide, [:ReactionID])
@@ -189,4 +105,148 @@ def get_reactions_from_species(species, mechanism)
     .order_by(:i)
     .select(Sequel[:ReactionsWide][:ReactionID], :Reaction, :Rate)
     .from_self(alias: :rea)
+end
+
+# rubocop:disable Metrics/AbcSize
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/ParameterLists
+# rubocop:disable Metrics/MethodLength
+def export_facsimile(species, rxns, rates, root_species, missing_peroxies, peroxies, generic: false)
+  # Make available to download
+  content_type 'text/plain'
+  filename = 'mcm_export.fac'
+  attachment filename
+
+  # Format sections for export
+  species_out = wrap_lines(species)
+  rxns_out = rxns.map { |row| "% #{row[:Rate]} : #{row[:Reaction]} ;\n" }.join
+  complex_rates_out = rates.map { |row| "#{row[:Child]} = #{row[:Definition]} ;\n" }.join
+  params_out = wrap_lines(root_species,
+                          starting_char: '* ',
+                          every_line_start: '* ',
+                          every_line_end: ' ;',
+                          ending_char: ' ;',
+                          sep: ' ')
+  missing_peroxies_out = wrap_lines(missing_peroxies,
+                                    starting_char: '* ',
+                                    every_line_start: '* ',
+                                    every_line_end: ' ;',
+                                    ending_char: ' ;',
+                                    sep: ' ')
+  peroxy_out = wrap_lines(peroxies,
+                          starting_char: 'RO2 = ',
+                          ending_char: ';',
+                          sep: ' + ',
+                          max_line_length: 65,
+                          every_line_start: ' ' * 6)
+
+  spacer = "#{'*' * 77} ;\n"
+  empty_comment = "*;\n"
+
+  citation_file = File.open("#{settings.public_folder}/citation.txt")
+  citation_lines = citation_file.readlines.map(&:chomp)
+
+  #---------------------- Write Facsimile file
+  # Citation comes first
+  out = ''
+  out += spacer
+  out += citation_lines.map { |row| "* #{row}\n" }.join
+  out += spacer
+
+  # Selected species + all species in this mechanism
+  out += spacer
+  out += params_out
+  out += empty_comment
+  out += "* Variable definitions. All species are listed here.;\n"
+  out += empty_comment
+  out += "VARIABLE\n"
+  out += species_out
+  out += spacer
+
+  # Complex rate coefficients
+  if generic
+    out += empty_comment
+    out += "* Generic Rate Coefficients ;\n"
+    out += empty_comment
+    out += "* Complex reactions ;\n"
+    out += empty_comment
+    out += complex_rates_out
+  end
+
+  # Peroxies
+  if peroxies.count.positive?
+    out += spacer
+    out += "* Peroxy radicals. ;\n*;\n"
+    if missing_peroxies.count.positive?
+      out += "* WARNING: The following species do not have SMILES strings in the database. ;\n"
+      out += "*          If any of these are peroxy radicals the RO2 sum will be wrong!!! ;\n"
+      out += missing_peroxies_out # TODO: Shoud this exclude inorganics?
+    end
+    out += spacer
+    out += empty_comment
+    out += peroxy_out
+    out += empty_comment
+  end
+
+  # Reactions
+  out += "* Reaction definitions. ;\n"
+  out += empty_comment
+  out += rxns_out
+  out += empty_comment
+
+  # Summary
+  out + "* End of Subset. No. of Species = #{species.count}, No. of Reactions = #{rxns.count} ;"
+  #---------------------- End write facsimile file
+end
+# rubocop:enable Metrics/AbcSize
+# rubocop:enable Metrics/CyclomaticComplexity
+# rubocop:enable Metrics/ParameterLists
+# rubocop:enable Metrics/MethodLength
+
+# rubocop:disable Metrics/MethodLength
+def extract_inorganic_submechanism
+  # Extracts inorganic reactions and the species involved within
+  #
+  # Args:
+  #   - None
+  #
+  # Returns:
+  #   - An object with attributes:
+  #     - rxns: Sequel Dataset with ReactionID, Reaction, Rate columns
+  #     - species: Sequel Dataset with Species column
+  rxns = DB[:ReactionsWide]
+         .exclude(InorganicCategory: nil)
+         .select(:ReactionID, :Reaction, :Rate)
+         .from_self(alias: :inorg)
+
+  reactants = rxns
+              .join(:Reactants, [:ReactionID])
+              .select(:Species)
+  products = rxns
+             .join(:Products, [:ReactionID])
+             .select(:Species)
+  species = reactants.union(products)
+  { species: species, rxns: rxns }
+end
+# rubocop:enable Metrics/MethodLength
+
+def export_default(*)
+  'Unknown format'
+end
+
+def exporter_factory(format)
+  # Returns the selected export format
+  #
+  # Args:
+  #   - format: String representing which format to use
+  #
+  # Returns:
+  #   A function that can export the mechanism
+  case format
+  when 'facsimile'
+    method(:export_facsimile)
+  else
+    puts "Unknown export format '#{format}'"
+    method(:export_default)
+  end
 end
