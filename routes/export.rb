c46af58a198f979b1b88a938a7318dadc405f50d
Stuart Lacy <stuart.lacy@york.ac.uk>
2023-09-14 11:27:11 +0100

    Refactoring

--- routes/export.rb
+++ routes/export.rb
@@ -250,3 +250,64 @@ def exporter_factory(format)
     method(:export_default)
   end
 end
+
+# rubocop:disable Metrics/MethodLength
+# rubocop:disable Metrics/AbcSize
+def traverse_complex_rates(parents)
+  # Traverses complex tokenized rates from parents (i.e. KMT04) down to children.
+  # Returns in order firstly by parent, and then by child depth
+  # (highest depth first, i.e. FCC, KCO, KCI, KRC, NC, FC, KFPAN)
+  # This is a breadth-first traversal, finding all root tokens, then all tokens at the next level down, etc...
+  #
+  # Args:
+  #   - parents: Array of strings with Token names to use as initial parents
+  #
+  # Returns:
+  #   - A dataset with 4 columns and 1 row per child token.
+  #     - RootToken: The root parent token
+  #     - Child: The child's token name
+  #     - Definition: The child's rate definition
+  #     - Depth: How many branches down the tree from the root parent was this child
+  depth = 0
+  new_children = DB[:Tokens]
+                 .where(Token: parents)
+                 .select(Sequel.lit("Token as RootToken, Token as Child, #{depth} as depth"))
+  all_tokens = new_children
+  # TODO: change to while loop with new children being empty
+  loop do
+    depth += 1
+    new_children = get_children_from_parent(new_children, DB)
+                   .select_append(Sequel.lit("#{depth} as depth"))
+    break if new_children.empty?
+
+    all_tokens = all_tokens.union(new_children)
+  end
+
+  # Get each child's rate Definition and limit each child to highest depth
+  # In SQLite when using Max or Min in a grouped select, it only returns the corresponding row with the max or min
+  # So no need to add a WHERE clause. Madness.
+  all_tokens = all_tokens
+               .distinct
+               .join(DB[:Tokens], Token: :Child)
+               .group_by(:RootToken, :Child, :Definition)
+               .select_append(Sequel.lit('max(depth) as Depth'))
+               .from_self(alias: :all)
+
+  # Identify which root tokens are simple or complex for later ordering
+  simple_generic_order = all_tokens
+                         .group_and_count(:RootToken)
+                         .from_self(alias: :sim)
+                         .select_append(Sequel.lit('count > 1 as IsComplex'))
+
+  # Want to order by 3 conditions:
+  #   - Whether simple or complex (simple has a tree with depth=1)
+  #   - By root parent token
+  #   - By depth of child token
+  all_tokens
+    .join(simple_generic_order, RootToken: :RootToken)
+    .order(:IsComplex, :RootToken, Sequel.desc(:depth))
+    .from_self(alias: :out)
+    .select(:RootToken, :Child, :Definition, :depth)
+end
+# rubocop:enable Metrics/MethodLength
+# rubocop:enable Metrics/AbcSize
