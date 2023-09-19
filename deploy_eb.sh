61be3d0d074a9a56c1e986693fb109fa0b959920
Stuart Lacy <stuart.lacy@york.ac.uk>
2023-09-19 15:02:31 +0100

    Working EBS deploy
    Outstanding features:
      - clean up the CF/deploy script so as to not need to
    hardcode resource IDs/ARNs
      - Only use a single param file for the CF template
      - Add a second environment to use for dev

--- deploy_eb.sh
+++ deploy_eb.sh
@@ -1,19 +1,22 @@
 # Login
 saml2aws login
 
+# TODO why do we have to have 2 parameter files?
+# Update this function to use param2.json
 find_param() {
   local keyName="$1"
   cat param.json | jq -r --arg keyName $keyName \
     '.[] | select(.ParameterKey == $keyName) | .ParameterValue'
 }
 
+VERSION=$(find_param Version)
 BUCKET_NAME=$(find_param S3Bucket)
-KEY_NAME=$(find_param S3Key)
+KEY_NAME="$(find_param S3Key)_${VERSION}.zip"
 
 # TODO ideally create Bucket in a separate CF and use output from that stack here, currently being manually created
-
+#
 # upload git archive to S3
-# TODO remove vendor from archive?
+# Once have progressed out of the initial development stage (i.e. when will be archiving comitted changes), can use git archive
 #git archive --format zip HEAD | aws s3 cp - s3://${BUCKET_NAME}/${KEY_NAME}
 zip -r - . -q -x ./.bundle/\* ./vendor/\* ./.git/\* | aws s3 cp - s3://${BUCKET_NAME}/${KEY_NAME}
 
@@ -24,14 +27,9 @@ aws cloudformation package --template-file eb_cloudformation.json --s3-bucket "$
 PARAMETER_FILE=param2.json
 PARAMETER_OVERRIDES="--parameter-overrides $(jq -r 'to_entries[] | "\(.key)=\"\(.value)\""' $PARAMETER_FILE | tr '\r\n' ' ')"
 
-# Original create stack code, have to manually delete to update stack so moved to copying the cfn-deploy-action code
-#aws cloudformation create-stack \
-#   --stack-name mcm-eb-dev \
-#   --template-body file://eb_cloudformation.json \
-#   --region eu-west-1 \
-#   --parameters file://param.json \
-#   --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
-
-#aws cloudformation deploy --stack-name $AWS_STACK_NAME --template-file $TEMPLATE_OUT $PARAMETER_OVERRIDES $CAPABILITIES $ROLE_ARN $FORCE_UPLOAD $TAGS $DEPLOY_BUCKET --no-fail-on-empty-changeset
 array=(aws cloudformation deploy --stack-name "$AWS_STACK_NAME" --template-file "$TEMPLATE_OUT" "$CAPABILITIES" "$PARAMETER_OVERRIDES" --force-upload --s3-bucket "$BUCKET_NAME" --no-fail-on-empty-changeset)
 eval $(echo ${array[@]})
+
+aws elasticbeanstalk create-application-version --application-name MCM --version-label $VERSION --description "v.$VERSION release" --source-bundle S3Bucket=$BUCKET_NAME,S3Key=$KEY_NAME
+# TODO how to get this environment ID from CF output?
+aws elasticbeanstalk update-environment --application-name MCM --version-label $VERSION --environment-id e-9xdvetkaa2 
