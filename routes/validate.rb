5b85f8a310a81153cc8c80c312ccaefd8ae6e8d5
Stuart Lacy <stuart.lacy@york.ac.uk>
2024-01-04 23:59:16 +0000

    Don't attempt to validate non-existant cookie (#254)

--- routes/validate.rb
+++ routes/validate.rb
@@ -2,12 +2,15 @@
 
 get '/:mechanism/marklist-validate' do
   raw_marklist = request.cookies['marklist']
-  raw_species = raw_marklist.split(',')
-  this_mechanism = params[:mechanism]
-  valid_species = DB[:SpeciesMechanisms]
-                  .where(Name: raw_species,
-                         Mechanism: this_mechanism)
-                  .select_map(:Name)
+  if raw_marklist.nil?
+    valid_species = []
+  else
+    raw_species = raw_marklist.split(',')
+    valid_species = DB[:SpeciesMechanisms]
+                    .where(Name: raw_species,
+                           Mechanism: params[:mechanism])
+                    .select_map(:Name)
+  end
   content_type :json
   { valid: valid_species }.to_json
 end
