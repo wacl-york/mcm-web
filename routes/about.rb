3c19cdfcd4ac66c91412b5c70cddc9de50027d73
Stuart Lacy <stuart.lacy@york.ac.uk>
2024-07-02 16:19:02 +0100

    Added page titles

--- routes/about.rb
+++ routes/about.rb
@@ -3,5 +3,6 @@
 get '/:mechanism/about/:file' do
   fn = File.join('public', 'static', params[:mechanism], "#{params[:file]}.html")
   @content = File.file?(fn) ? File.read(fn) : "<h1>Error</h1><p>Unknown page '#{params[:file]}'.</p>"
+  @title = "#{params[:mechanism]} - #{params[:file].capitalize}"
   erb :about
 end
