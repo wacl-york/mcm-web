5d8721f02ae2ba2c83e0e0caedbf8c9749fb5dd7
Viktor Czyzewski <viktor.czyzewski@york.ac.uk>
2024-07-30 13:30:16 +0100

    Fixed query parameters not being recognised

--- routes/advanced_search.rb
+++ routes/advanced_search.rb
@@ -1,16 +1,11 @@
 # frozen_string_literal: true
 
 get '/:mechanism/advanced_search' do
-  q = params[:q]
-  puts q
-  @species = if q.nil?
-               # nil
-               MCM::Search::Advanced.search(nil, @mechanism)
+  @species = if params.size > 1
+              MCM::Search::Advanced.search(params, @mechanism)
              else
-               MCM::Search::Advanced.search(q, @mechanism)
+              nil
              end
-
-  puts "output: #{@species.class}"
 
   @title = "#{params[:mechanism]} - Advanced search"
   erb :advanced_search
